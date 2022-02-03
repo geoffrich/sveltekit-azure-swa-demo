376340dcee289c9c3e96b59ff8390a6092cb7ab9
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-02-03 11:58:24 -0800

    Update to adapter v0.4

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,14 @@
-import azure from 'svelte-adapter-azure-swa';
+import adapter from 'svelte-adapter-azure-swa';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: azure()
+		adapter: adapter(),
+
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		}
 	}
 };
 
